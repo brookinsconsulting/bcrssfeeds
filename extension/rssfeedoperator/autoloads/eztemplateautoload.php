bf5b98b0df41ec1c730e54cb8ac3d8467435829c
Bertrand Dunogier <bdunogier@users.noreply.github.com>
2008-07-24 09:25:28 +0000

    -- initial import of version 1.0

---
+++ extension/rssfeedoperator/autoloads/eztemplateautoload.php
@@ -0,0 +1,6 @@
+<?php
+$eZTemplateOperatorArray = array();
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/rssfeedoperator/autoloads/ezrssfeedoperator.php',
+                                    'class' => 'eZRSSFeedOperator',
+                                    'operator_names' => array( 'rssfeed' ) );
+?>
